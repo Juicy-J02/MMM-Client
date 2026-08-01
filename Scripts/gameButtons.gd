2cfea4b6cd2fef779b952545ffad89c3af77606f
Jackson Gray <jrgray609@gmail.com>
2026-08-01 16:08:07 -0600

    Delete Scripts directory

--- Scripts/gameButtons.gd
+++
@@ -1,4 +0,0 @@
-extends CanvasLayer
-
-func _on_exit_pressed():
-	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
--- Scripts/global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var auth_token := ""
-var current_user
-
-var deck = []
-var combo_character = ""
-
-var loaded_deck
